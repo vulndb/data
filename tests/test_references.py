2bae42ffdc19ee76436d6eb4b7e7e31324ae80a3
Anthon Pang <apang@softwaredevelopment.ca>
2015-07-28 21:30:48 -0400

    Add docstring

--- tests/test_references.py
+++ tests/test_references.py
@@ -3,8 +3,15 @@ import re
 from vulndb import DBVuln
 from tests.vulndb_test import VulnDBTest
 
-class TestReferences(VulnDBTest):
+CVE_URL_PATTERN = '^http://cwe.mitre.org/data/definitions/[0-9]+\.html$'
 
+class TestReferences(VulnDBTest):
+    """
+    We don't want redundant references.  Test for the presence of a
+    reference URL that contains a cve.mitre.org URL.  If an invalid
+    reference is detected, simply remove the reference and add the
+    CWE-ID to the "cwe" section of the vulnerability.
+    """
     def test_no_redundant_cve_mitre_org_urls(self):
         invalid = []
 
@@ -12,7 +19,7 @@ class TestReferences(VulnDBTest):
             db_vuln = DBVuln.from_id(vuln_id)
 
             for reference in db_vuln.references:
-                if re.match('^http://cwe.mitre.org/data/definitions/[0-9]+\.html$', reference.url):
+                if re.match(CVE_URL_PATTERN, reference.url):
                     invalid.append(reference.url)
 
         self.assertEqual(invalid, [])
